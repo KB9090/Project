3e89f2cf3258246e4b66f6c7653c170bcd815eb0
KB9090 <ZiqIz90@gmail.com>
2023-03-17 23:47:12 +0800

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SpotifyService } from './spotify.service';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+@NgModule
+({
+  declarations:
+  [
+  AppComponent,
+  NavbarComponent,
+  HomeComponent,
+  AboutComponent,
+  SearchComponent],
+
+  imports:[BrowserModule,AppRoutingModule,HttpClientModule,FormsModule],
+  providers: [SpotifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
